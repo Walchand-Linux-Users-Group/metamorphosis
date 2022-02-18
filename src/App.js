c5054f969017be5797c7410986a35af3c7701a27
shaikhamaan <amaan.shaikh@walchandsangli.ac.in>
2022-02-18 14:56:36 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Registration from "./components/form";
 
 function App() {
   return (
-    <div className="App col-lg-6 col-md-6 col-sm-10 col-xs-5 m-auto ">
+    <div className="App col-lg-6 col-md-6 col-sm-10 col-10 m-auto mt-2 mb-4 ">
       <Registration />
     </div>
   );
